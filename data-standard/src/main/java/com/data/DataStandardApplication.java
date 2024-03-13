d6710a60354cdf3a2e6a3147516535d9a07f4d4c
Zhangr132 <zhangr132@git.com>
2024-03-13 11:04:57 +0800

    初步搭建项目

---
+++ data-standard/src/main/java/com/data/DataStandardApplication.java
@@ -0,0 +1,13 @@
+package com.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStandardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStandardApplication.class, args);
+    }
+
+}
